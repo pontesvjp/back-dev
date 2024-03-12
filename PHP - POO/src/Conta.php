2eecca519cdd7f12c49823ef7f105f471ea330d2
pontesvjp <pontes.vjp@gmail.com>
2024-03-12 18:17:20 -0300

    aula 2

--- PHP - POO/src/Conta.php
+++ PHP - POO/src/Conta.php
@@ -1,8 +1,35 @@
 <?php
+class Conta
+{
+    public $cpfTitular;
+    public $nomeTitular;
+    public $saldo = 0;
 
-class Conta {
-    public string $cpfTitular;
-    public string $nomeTitular;
-    public float $saldoo; 
+    public function sacar(float $valorASacar)
+    {
+        if ($valorASacar > $this->saldo) {
+            echo "saldo indisponivel";
+            return;
+        }
+        $this->saldo -= $valorASacar;
+    }
+
+    public function depositar(float $valorADepositar)
+    {
+        if ($valorADepositar < 0) {
+            echo 'vc precisa depositar mais';
+            return;
+        }
+        $this->saldo += $valorADepositar;
+    }
+
+    public function tranferir(float $valorATransferir, Conta $contaDestino): void
+    {
+        if ($valorATransferir > $this->saldo) {
+            echo 'valor indisponivel';
+            return;
+        }
+        $this->sacar($valorATransferir);
+        $contaDestino->depositar($valorATransferir);
+    }
 }
-
