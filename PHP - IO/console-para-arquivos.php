d5419fd3055cf39a4dcbb8ea69d1467bb697de67
pontesvjp <pontes.vjp@gmail.com>
2024-03-18 17:36:49 -0300

    aula 4

---
+++ PHP - IO/console-para-arquivos.php
@@ -0,0 +1,7 @@
+<?php
+
+
+
+$novoCurso - trim(fgets(STDIN));
+
+file_put_contents('cursos-php.txt', "/n$novoCurso", FILE_APPEND);
--- PHP - IO/times-sccp.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$arquivo = fopen('PHP - IO\times-sccp.txt', 'a');
-
-$linha = PHP_EOL . "1990 – O time de Neto";
-
-fwrite($arquivo, $linha);
-
-fclose($arquivo);
