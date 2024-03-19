bc4f63257bb78a8956dae57623841b494d9a046d
pontesvjp <pontes.vjp@gmail.com>
2024-03-19 17:12:21 -0300

    TRATAMENTO DE EXCESSOES

--- PHP - POO/PHP I/O/leitor-sccp.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$lista = file_get_contents('PHP I\O\jogadores-sccp.txt');
-
-echo $lista;
---
+++ PHP Exceptions/pilha.php
@@ -0,0 +1,34 @@
+<?php
+
+function funcao1()
+{
+    echo 'Entrei na função 1' . PHP_EOL;
+
+    try {
+        funcao2();
+    } catch (RuntimeException | DivisionByZeroError $problema) {
+        echo "na funçao 1, resolvi a função 2" . PHP_EOL;
+    }
+}
+echo 'Saindo da função 1' . PHP_EOL;
+
+function funcao2()
+{
+    echo 'Entrei na função 2' . PHP_EOL;
+
+
+    $divisao = intdiv(5, 0);
+    $arrayFixo = new SplFixedArray(2);
+    $arrayFixo[3] = 'Valor';
+    echo "problema na funcção 2" . PHP_EOL;
+
+
+    for ($i = 1; $i <= 5; $i++) {
+        echo $i . PHP_EOL;
+    }
+    echo 'Saindo da função 2' . PHP_EOL;
+}
+
+echo 'Iniciando o programa principal' . PHP_EOL;
+funcao1();
+echo 'Finalizando o programa principal' . PHP_EOL;
