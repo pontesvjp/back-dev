6dc8ea59a9431486b8a247051100bc73538b8960
pontesvjp <pontes.vjp@gmail.com>
2024-04-06 07:27:06 -0300

    dfdfd

--- PHP - PDO/inserir-aluno.php
+++ PHP - PDO/inserir-aluno.php
@@ -15,7 +15,7 @@ $name = $student->name();
 
 $sqlInsert = "INSERT INTO students (name, birth_date) VALUES (:name, :birth_date);";
 $statement = $pdo->prepare($sqlInsert);
-$statement->bindParam(':name', $student->name());
+$statement->bindParam(':naddme', $student->name());
 $statement->bindValue(':birth_date', $student->birthDate()->format('Y-m-d'));
 
 if ($statement->execute()) {
