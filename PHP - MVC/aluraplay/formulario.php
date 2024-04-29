779de7055d661931d69b76cd4ab53b1827a48388
pontesvjp <pontes.vjp@gmail.com>
2024-04-29 17:53:02 -0300

    sla

--- PHP - MVC/aluraplay/formulario.php
+++ PHP - MVC/aluraplay/formulario.php
@@ -15,7 +15,8 @@ if ($id !== false) {
     $video = $statement->fetch(\PDO::FETCH_ASSOC);
 }
 
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html lang="pt-br">
 
 <head>
@@ -49,31 +50,19 @@ if ($id !== false) {
 
     <main class="container">
 
-        <form class="container__formulario"
-              action="<?= $id === false ? '/novo-video.php' : '/editar-video.php?id=' . $id; ?>"
-              method="post">
+        <form class="container__formulario" method="post">
             <h2 class="formulario__titulo">Envie um vídeo!</h2>
-                <div class="formulario__campo">
-                    <label class="campo__etiqueta" for="url">Link embed</label>
-                    <input name="url"
-                           value="<?= $video['url']; ?>"
-                           class="campo__escrita"
-                           required
-                           placeholder="Por exemplo: https://www.youtube.com/embed/FAY1K2aUg5g"
-                           id='url' />
-                </div>
+            <div class="formulario__campo">
+                <label class="campo__etiqueta" for="url">Link embed</label>
+                <input name="url" value="<?= $video['url']; ?>" class="campo__escrita" required placeholder="Por exemplo: https://www.youtube.com/embed/FAY1K2aUg5g" id='url' />
+            </div>
 
-                <div class="formulario__campo">
-                    <label class="campo__etiqueta" for="titulo">Titulo do vídeo</label>
-                    <input name="titulo"
-                           value="<?= $video['title']; ?>"
-                           class="campo__escrita"
-                           required
-                           placeholder="Neste campo, dê o nome do vídeo"
-                           id='titulo' />
-                </div>
+            <div class="formulario__campo">
+                <label class="campo__etiqueta" for="titulo">Titulo do vídeo</label>
+                <input name="titulo" value="<?= $video['title']; ?>" class="campo__escrita" required placeholder="Neste campo, dê o nome do vídeo" id='titulo' />
+            </div>
 
-                <input class="formulario__botao" type="submit" value="Enviar" />
+            <input class="formulario__botao" type="submit" value="Enviar" />
         </form>
 
     </main>
