0779e92e8fed787ae7b4dd0dd84036965a8dcabe
pontesvjp <pontes.vjp@gmail.com>
2024-05-05 12:12:13 -0300

    sei la

---
+++ PHP - MVC/aluraplay/novo-video.php
@@ -0,0 +1,25 @@
+<?php
+
+require "conexao-bd.php";
+$url = filter_input(INPUT_POST, 'url', FILTER_VALIDATE_URL);
+if ($url === false) {
+    header('Location: /index.php?sucesso=0');
+    exit();
+}
+$titulo = filter_input(INPUT_POST, 'titulo');
+if ($titulo === false) {
+    header('Location: /index.php?sucesso=0');
+    exit();
+}
+
+$sql = 'INSERT INTO videos (url, title) VALUES (?, ?)';
+$statement = $pdo->prepare($sql);
+$statement->bindValue(1, $url);
+$statement->bindValue(2, $titulo);
+
+if ($statement->execute() === false) {
+    header('Location: /index.php?sucesso=0');
+} else {
+    header('Location: /index.php?sucesso=1');
+}
+
