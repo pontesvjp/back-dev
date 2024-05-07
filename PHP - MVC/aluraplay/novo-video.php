a1fe8d837fdceb9c364dd662521431ee8935f460
pontesvjp <pontes.vjp@gmail.com>
2024-05-07 13:22:52 -0300

    cap3

--- PHP - MVC/aluraplay/novo-video.php
+++ PHP - MVC/aluraplay/novo-video.php
@@ -3,12 +3,12 @@
 require "conexao-bd.php";
 $url = filter_input(INPUT_POST, 'url', FILTER_VALIDATE_URL);
 if ($url === false) {
-    header('Location: /index.php?sucesso=0');
+    header('Location: /?sucesso=0');
     exit();
 }
 $titulo = filter_input(INPUT_POST, 'titulo');
 if ($titulo === false) {
-    header('Location: /index.php?sucesso=0');
+    header('Location: /?sucesso=0');
     exit();
 }
 
@@ -18,8 +18,8 @@ $statement->bindValue(1, $url);
 $statement->bindValue(2, $titulo);
 
 if ($statement->execute() === false) {
-    header('Location: /index.php?sucesso=0');
+    header('Location: /?sucesso=0');
 } else {
-    header('Location: /index.php?sucesso=1');
+    header('Location: /?sucesso=1');
 }
 
