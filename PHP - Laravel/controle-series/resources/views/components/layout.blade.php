d8e9a39795ac5cdfa9e12da24591f4c154abc5d1
pontesvjp <pontes.vjp@gmail.com>
2024-05-30 15:03:30 -0300

    pt 1 laravel

--- PHP - Laravel/controle-series/resources/views/components/layout.blade.php
+++ PHP - Laravel/controle-series/resources/views/components/layout.blade.php
@@ -4,11 +4,14 @@
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{{ $title }} - Controle de Séries</title>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
     </head>
 
     <body>
-        <h1>{{ $title }}</h1>
+        <div class="container">
+            <h1>{{ $title }}</h1>
 
-        {{ $slot }}
+            {{ $slot }}
+        </div>
     </body>
 </html>
--- PHP - Laravel/controle-series/resources/views/series/create.blade.php
+++ PHP - Laravel/controle-series/resources/views/series/create.blade.php
@@ -1,6 +1,9 @@
 <x-layout title="Nova Série">
-    <form method="post" action>
-        <label for="nome">Nome:</label>
-        <input type="text" name="nome" id="nome" />
-    </form>
+    <form method="post" action="/series/salvar">
+        @csrf
+        <div class="mb-3">
+        <label class=" form-label"for="nome">Nome:</label>
+        <input type="text" name="nome" class="form-control" id="nome" />
+    </div>
+    <button type="submit" class="btn btn-dark">Adcionar</button> </form>
 </x-layout>
