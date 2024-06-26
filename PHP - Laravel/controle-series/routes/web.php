b6c0a614446c43d76a2095c92be4af13e504b134
pontesvjp <pontes.vjp@gmail.com>
2024-06-26 18:23:24 -0300

    Laravel: crie aplicações web em PHP

--- PHP - Laravel/controle-series/routes/web.php
+++ PHP - Laravel/controle-series/routes/web.php
@@ -1,29 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\SeriesController;
-use App\Http\Controllers\SeasonsController;
-use App\Http\Controllers\EpisodesController;
-use App\Http\Controllers\LoginController;
-use App\Http\Middleware\Autenticator;
-use Illuminate\Http\Request;
-use App\Http\Controllers\UsersController;
 
-Route::resource('/series', SeriesController::class)->except(['show']);
-
-
-Route::middleware(Autenticator::class)->group(function () {
-    Route::get('/', function () {
-        return redirect('/series');
-    })->middleware(Autenticator::class);
-    Route::get('/series/{series}/season', [SeasonsController::class, 'index'])->name('seasons.index');
-    Route::get('/seasons/{season}/episodes', [EpisodesController::class, 'index'])->name('episodes.index');
-    Route::post('/seasons/{season}/episodes', [EpisodesController::class, 'update'])->name('episodes.update');
+Route::get('/', function () {
+    return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/login', [LoginController::class, 'index'])->name('login');
-Route::post('/login', [LoginController::class, 'store'])->name('signin');
-Route::get('/logout', [LoginController::class, 'destroy'])->name('logout');
-Route::get('/register', [UsersController::class, 'create'])->name('users.create');
-Route::post('/register', [UsersController::class, 'store'])->name('users.store');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
