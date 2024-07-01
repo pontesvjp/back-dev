dcc761df2a47f65b3009a58fdb4550c4d8f6538f
pontesvjp <pontes.vjp@gmail.com>
2024-07-01 16:40:50 -0300

    Criando um email

--- PHP - Laravel/controle-series/routes/web.php
+++ PHP - Laravel/controle-series/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Mail\SeriesCreated;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -17,4 +18,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+Route::get('/email', function () {
+    return new SeriesCreated('Série de teste', 1, 5, 10);
+});
+
+require __DIR__ . '/auth.php';
